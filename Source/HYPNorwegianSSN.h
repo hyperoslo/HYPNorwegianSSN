9673fb90b894ffd832503051c20a0b6d35e75135
Christoffer Winterkvist <chris@zenangst.com>
2014-10-05 19:37:44 +0200

    Add core class

---
+++ Source/HYPNorwegianSSN.h
@@ -0,0 +1,13 @@
+//
+//  HYPNorwegianSSN.h
+//  HYPNorwegianSSN
+//
+//  Created by Christoffer Winterkvist on 10/5/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HYPNorwegianSSN : NSObject
+
+@end
---
+++ Source/HYPNorwegianSSN.m
@@ -0,0 +1,13 @@
+//
+//  HYPNorwegianSSN.m
+//  HYPNorwegianSSN
+//
+//  Created by Christoffer Winterkvist on 10/5/14.
+//
+//
+
+#import "HYPNorwegianSSN.h"
+
+@implementation HYPNorwegianSSN
+
+@end
